dcb031885911c8e5d7acb2a49891afbd7ff6b324
Damares Emanoele <damares290619@gmail.com>
2026-08-31 09:33:27 -0300

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
+
 export function App() {
   console.log('Oi');
 
   return (
     <>
       <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá mundo1!
+      </Heading>
+
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci quo
         amet tempore voluptate debitis fugiat nesciunt. Velit illum fugiat
