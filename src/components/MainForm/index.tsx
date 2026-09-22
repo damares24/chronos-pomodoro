5658149aad879eb586fbef017caccf4890e6e525
Damares Emanoele <damares290619@gmail.com>
2026-09-22 09:51:38 -0300

    O conceito de prop drilling (talvez você precise de Context API)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,28 +2,10 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import type { HomeProps } from '../../pages/Home';
 
-export function MainForn({ state, setState }: HomeProps) {
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          workTime: 34,
-        },
-        formattedSecondsRemaining: '23:34',
-      };
-    });
-  }
+export function MainForn() {
   return (
     <form className='form' action=''>
-      <div>
-        <button type='button' onClick={handleClick}>
-          Clicar
-        </button>
-      </div>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
@@ -34,7 +16,7 @@ export function MainForn({ state, setState }: HomeProps) {
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de {state.config.workTime}</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
       <div className='formRow'>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,17 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForn } from '../../components/MainForm';
-import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export type HomeProps = {
-  state: TaskStateModel;
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
-};
-
-export function Home(props: HomeProps) {
+export function Home() {
   return (
     <MainTemplate>
       <Container>
-        <CountDown {...props} />
+        <CountDown />
       </Container>
 
       <Container>
-        <MainForn {...props} />
+        <MainForn />
       </Container>
     </MainTemplate>
   );
